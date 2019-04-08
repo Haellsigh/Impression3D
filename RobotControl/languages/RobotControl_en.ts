0f3602b743cc04034809e22ec272b3ef145b8ef4
Azais Corentin <azaiscorentin@gmail.com>
2019-04-08 21:50:29 +0200

    Mise à jour des traductions

--- RobotControl/languages/RobotControl_en.ts
+++ RobotControl/languages/RobotControl_en.ts
@@ -44,7 +44,7 @@
     </message>
     <message>
         <location filename="../mainwindow.ui" line="89"/>
-        <location filename="../mainwindow.cpp" line="128"/>
+        <location filename="../mainwindow.cpp" line="112"/>
         <source>Error</source>
         <translation></translation>
     </message>
@@ -104,14 +104,96 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="599"/>
+        <location filename="../mainwindow.ui" line="359"/>
+        <location filename="../mainwindow.ui" line="458"/>
+        <location filename="../mainwindow.ui" line="523"/>
+        <source> * 0.0001 degrees</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="384"/>
+        <location filename="../mainwindow.ui" line="785"/>
+        <source>Position Y</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="398"/>
+        <location filename="../mainwindow.ui" line="846"/>
+        <source>Rotation Y</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="405"/>
+        <location filename="../mainwindow.ui" line="768"/>
+        <source>Position X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="476"/>
+        <location filename="../mainwindow.ui" line="819"/>
+        <source>Rotation X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="483"/>
+        <location filename="../mainwindow.ui" line="744"/>
+        <source>Rotation Z</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="697"/>
         <source>Current position</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="432"/>
-        <location filename="../mainwindow.ui" line="716"/>
-        <source>Pulse</source>
+        <location filename="../mainwindow.ui" line="377"/>
+        <location filename="../mainwindow.ui" line="751"/>
+        <source>Position Z</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="864"/>
+        <source>Print a file</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="898"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Step 1:&lt;/span&gt; Set the origin using the &apos;Manual Control&apos; tab then click the button to save it&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="918"/>
+        <source>Set print origin</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="927"/>
+        <source>Print origin: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1003"/>
+        <source>Select a gcode file by clicking &apos;Browse&apos;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1010"/>
+        <source>Browse...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1022"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Step 2:&lt;/span&gt; Select the .gcode file you want to print&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1055"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Step 3:&lt;/span&gt; Click print to begin the printing process&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1075"/>
+        <source>Print</source>
         <translation></translation>
     </message>
     <message>
@@ -120,109 +202,103 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="341"/>
-        <location filename="../mainwindow.ui" line="371"/>
-        <location filename="../mainwindow.ui" line="401"/>
-        <location filename="../mainwindow.ui" line="508"/>
-        <location filename="../mainwindow.ui" line="538"/>
-        <location filename="../mainwindow.ui" line="568"/>
+        <location filename="../mainwindow.ui" line="574"/>
+        <location filename="../mainwindow.ui" line="625"/>
+        <location filename="../mainwindow.ui" line="676"/>
         <source> µm</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="416"/>
+        <location filename="../mainwindow.ui" line="391"/>
         <source>Send position</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="423"/>
-        <location filename="../mainwindow.ui" line="626"/>
-        <source>Axis 1</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="442"/>
-        <location filename="../mainwindow.ui" line="726"/>
-        <source>Base cartesian</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="449"/>
-        <location filename="../mainwindow.ui" line="733"/>
-        <source>User cartesian</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="458"/>
-        <location filename="../mainwindow.ui" line="643"/>
-        <source>Axis 5</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="465"/>
-        <location filename="../mainwindow.ui" line="619"/>
-        <source>Axis 4</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="472"/>
-        <location filename="../mainwindow.ui" line="612"/>
-        <source>Axis 3</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="479"/>
-        <location filename="../mainwindow.ui" line="605"/>
-        <source>Axis 2</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="486"/>
-        <location filename="../mainwindow.ui" line="650"/>
-        <source>Axis 6</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="807"/>
+        <location filename="../mainwindow.ui" line="1112"/>
         <source>File</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="812"/>
+        <location filename="../mainwindow.ui" line="1117"/>
         <source>Tools</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="816"/>
-        <location filename="../mainwindow.ui" line="827"/>
+        <location filename="../mainwindow.ui" line="1121"/>
+        <location filename="../mainwindow.ui" line="1132"/>
         <source>Language</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="134"/>
-        <source>No error</source>
+        <location filename="../mainwindow.cpp" line="25"/>
+        <source>Print status: %p% (%v steps / %m)</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="130"/>
+        <location filename="../mainwindow.cpp" line="114"/>
         <source>Error: code %1</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="205"/>
+        <location filename="../mainwindow.cpp" line="118"/>
+        <source>No errors</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="190"/>
         <source>Current language changed to %1</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="224"/>
-        <source>Loading language</source>
+        <location filename="../mainwindow.cpp" line="209"/>
+        <source>Loading language %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="240"/>
+        <source>Logs</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="281"/>
+        <source>Select GCode file</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="281"/>
+        <source>GCode Files (*.g *.gc *.gcode)</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>dx200::GCodeInterpreter</name>
+    <message>
+        <location filename="../robot/gcodeinterpreter.cpp" line="58"/>
+        <location filename="../robot/gcodeinterpreter.cpp" line="108"/>
+        <location filename="../robot/gcodeinterpreter.cpp" line="141"/>
+        <source>GCode Interpreter:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../robot/gcodeinterpreter.cpp" line="102"/>
+        <source>GCode Interpreter: unknown command </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../robot/gcodeinterpreter.cpp" line="141"/>
+        <source> unknown command </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../robot/gcodeinterpreter.cpp" line="148"/>
+        <source>Execution time: %1s</source>
         <translation></translation>
     </message>
 </context>
 <context>
     <name>gcode::Reader</name>
     <message>
-        <location filename="../gcode/gcodereader.cpp" line="62"/>
+        <location filename="../gcode/gcodereader.cpp" line="61"/>
         <source>Can&apos;t open file %1</source>
         <translation></translation>
     </message>
--- RobotControl/languages/RobotControl_fr.ts
+++ RobotControl/languages/RobotControl_fr.ts
@@ -44,7 +44,7 @@
     </message>
     <message>
         <location filename="../mainwindow.ui" line="89"/>
-        <location filename="../mainwindow.cpp" line="128"/>
+        <location filename="../mainwindow.cpp" line="112"/>
         <source>Error</source>
         <translation>Erreur</translation>
     </message>
@@ -104,15 +104,97 @@
         <translation>Contrôle manuel</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="599"/>
+        <location filename="../mainwindow.ui" line="359"/>
+        <location filename="../mainwindow.ui" line="458"/>
+        <location filename="../mainwindow.ui" line="523"/>
+        <source> * 0.0001 degrees</source>
+        <translation> * 0.0001 degrés</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="384"/>
+        <location filename="../mainwindow.ui" line="785"/>
+        <source>Position Y</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="398"/>
+        <location filename="../mainwindow.ui" line="846"/>
+        <source>Rotation Y</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="405"/>
+        <location filename="../mainwindow.ui" line="768"/>
+        <source>Position X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="476"/>
+        <location filename="../mainwindow.ui" line="819"/>
+        <source>Rotation X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="483"/>
+        <location filename="../mainwindow.ui" line="744"/>
+        <source>Rotation Z</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="697"/>
         <source>Current position</source>
         <translation>Position actuelle</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="432"/>
-        <location filename="../mainwindow.ui" line="716"/>
-        <source>Pulse</source>
+        <location filename="../mainwindow.ui" line="377"/>
+        <location filename="../mainwindow.ui" line="751"/>
+        <source>Position Z</source>
         <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="864"/>
+        <source>Print a file</source>
+        <translation>Imprimer un fichier</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="898"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Step 1:&lt;/span&gt; Set the origin using the &apos;Manual Control&apos; tab then click the button to save it&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Etape 1:&lt;/span&gt; Réglez l&apos;origine à l&apos;aide de l&apos;onglet &quot;Contrôle Manuel&quot; puis cliquez sur le bouton pour l&apos;enregistrer&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="918"/>
+        <source>Set print origin</source>
+        <translation>Définir l&apos;origine d&apos;impression</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="927"/>
+        <source>Print origin: </source>
+        <translation>Origine d&apos;impression: </translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1003"/>
+        <source>Select a gcode file by clicking &apos;Browse&apos;</source>
+        <translation>Sélectionnez un fihciher gcode en cliquant sur &apos;Parcourir&apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1010"/>
+        <source>Browse...</source>
+        <translation>Parcourir...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1022"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Step 2:&lt;/span&gt; Select the .gcode file you want to print&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Etape 2:&lt;/span&gt; Sélectionnez le fichier gcode que vous souhaitez imprimer&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1055"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Step 3:&lt;/span&gt; Click print to begin the printing process&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Etape 3:&lt;/span&gt;Cliquez sur &apos;Imprimer&apos; pour commencer l&apos;impression&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1075"/>
+        <source>Print</source>
+        <translation>Imprimer</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="320"/>
@@ -120,109 +202,103 @@
         <translation>Mouvements mauels</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="341"/>
-        <location filename="../mainwindow.ui" line="371"/>
-        <location filename="../mainwindow.ui" line="401"/>
-        <location filename="../mainwindow.ui" line="508"/>
-        <location filename="../mainwindow.ui" line="538"/>
-        <location filename="../mainwindow.ui" line="568"/>
+        <location filename="../mainwindow.ui" line="574"/>
+        <location filename="../mainwindow.ui" line="625"/>
+        <location filename="../mainwindow.ui" line="676"/>
         <source> µm</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="416"/>
+        <location filename="../mainwindow.ui" line="391"/>
         <source>Send position</source>
         <translation>Envoyer la position</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="423"/>
-        <location filename="../mainwindow.ui" line="626"/>
-        <source>Axis 1</source>
-        <translation>Axe 1</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="442"/>
-        <location filename="../mainwindow.ui" line="726"/>
-        <source>Base cartesian</source>
-        <translation>Base cartésien</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="449"/>
-        <location filename="../mainwindow.ui" line="733"/>
-        <source>User cartesian</source>
-        <translation>Utilisateur cartésien</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="458"/>
-        <location filename="../mainwindow.ui" line="643"/>
-        <source>Axis 5</source>
-        <translation>Axe 5</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="465"/>
-        <location filename="../mainwindow.ui" line="619"/>
-        <source>Axis 4</source>
-        <translation>Axe 4</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="472"/>
-        <location filename="../mainwindow.ui" line="612"/>
-        <source>Axis 3</source>
-        <translation>Axe 3</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="479"/>
-        <location filename="../mainwindow.ui" line="605"/>
-        <source>Axis 2</source>
-        <translation>Axe 2</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="486"/>
-        <location filename="../mainwindow.ui" line="650"/>
-        <source>Axis 6</source>
-        <translation>Axe 6</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.ui" line="807"/>
+        <location filename="../mainwindow.ui" line="1112"/>
         <source>File</source>
         <translation>Fichier</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="812"/>
+        <location filename="../mainwindow.ui" line="1117"/>
         <source>Tools</source>
         <translation>Outils</translation>
     </message>
     <message>
-        <location filename="../mainwindow.ui" line="816"/>
-        <location filename="../mainwindow.ui" line="827"/>
+        <location filename="../mainwindow.ui" line="1121"/>
+        <location filename="../mainwindow.ui" line="1132"/>
         <source>Language</source>
         <translation>Langage</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="134"/>
-        <source>No error</source>
-        <translation>Aucune erreur</translation>
+        <location filename="../mainwindow.cpp" line="25"/>
+        <source>Print status: %p% (%v steps / %m)</source>
+        <translation>Etat de l&apos;impression: %p% (%v étapes / %m)</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="130"/>
+        <location filename="../mainwindow.cpp" line="114"/>
         <source>Error: code %1</source>
         <translation>Erreur: code %1</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="205"/>
+        <location filename="../mainwindow.cpp" line="118"/>
+        <source>No errors</source>
+        <translation>Aucune erreur</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="190"/>
         <source>Current language changed to %1</source>
         <translation>Langage actuel changé vers %1</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="224"/>
-        <source>Loading language</source>
-        <translation>Chargement de la langue</translation>
+        <location filename="../mainwindow.cpp" line="209"/>
+        <source>Loading language %1</source>
+        <translation>Chargement de la langue %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="240"/>
+        <source>Logs</source>
+        <translation>Journaux</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="281"/>
+        <source>Select GCode file</source>
+        <translation>Sélectionnez un fichier gcode</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="281"/>
+        <source>GCode Files (*.g *.gc *.gcode)</source>
+        <translation>Fichiers GCode (*.g *.gc *.gcode)</translation>
+    </message>
+</context>
+<context>
+    <name>dx200::GCodeInterpreter</name>
+    <message>
+        <location filename="../robot/gcodeinterpreter.cpp" line="58"/>
+        <location filename="../robot/gcodeinterpreter.cpp" line="108"/>
+        <location filename="../robot/gcodeinterpreter.cpp" line="141"/>
+        <source>GCode Interpreter:</source>
+        <translation>Interpréteur de GCode:</translation>
+    </message>
+    <message>
+        <location filename="../robot/gcodeinterpreter.cpp" line="102"/>
+        <source>GCode Interpreter: unknown command </source>
+        <translation>Interpréteur de GCode: commande inconnue </translation>
+    </message>
+    <message>
+        <location filename="../robot/gcodeinterpreter.cpp" line="141"/>
+        <source> unknown command </source>
+        <translation> commande inconnue </translation>
+    </message>
+    <message>
+        <location filename="../robot/gcodeinterpreter.cpp" line="148"/>
+        <source>Execution time: %1s</source>
+        <translation>Temps d&apos;exécution: %1s</translation>
     </message>
 </context>
 <context>
     <name>gcode::Reader</name>
     <message>
-        <location filename="../gcode/gcodereader.cpp" line="62"/>
+        <location filename="../gcode/gcodereader.cpp" line="61"/>
         <source>Can&apos;t open file %1</source>
         <translation>Impossible d&apos;ouvrir le fichier %1</translation>
     </message>
